8b8dc14f40d7a266ced16b2e23c1c5826706d3e1
Rahul Batra <spyruf@gmail.com>
2017-12-19 22:41:01 -0500

    changed to selenium

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import requests
+import selenium
 import time
-from bs4 import BeautifulSoup
 
 # Config
 url = "https://www.grailed.com/feed/rn0qT30h5A"
-
-response = requests.get(url)
-print(response)
