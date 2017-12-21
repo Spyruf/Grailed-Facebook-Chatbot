8f62cb239c649604e2be04d637f59c5f1ea7edd5
Rahul Batra <spyruf@gmail.com>
2017-12-21 05:30:45 -0500

    changing method

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
-import selenium
 import time
+from selenium import webdriver
+from bs4 import BeautifulSoup as bs
+from colorama import Fore, Back, Style
 
 # Config
 url = "https://www.grailed.com/feed/rn0qT30h5A"
+
+# Set Up
+print(Fore.GREEN + "Start" + Style.RESET_ALL)
+
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+driver = webdriver.Chrome(chrome_options=options)
+
+# Code
+driver.get(url)
+
+html = driver.page_source
+soup = bs(html, "html.parser")
+
+row = soup.find_all("div", {"class": "row"})
+
+for listing in row:
+    print(listing.div.a.get("href"))
+
+driver.quit()
+print(Fore.GREEN + "End" + Style.RESET_ALL)
