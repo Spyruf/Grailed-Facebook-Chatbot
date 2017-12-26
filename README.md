8253e48adc52622e808f95c4e628133ec5bda7bc
Rahul Batra <spyruf@gmail.com>
2017-12-25 23:06:03 -0500

    working on threading for multiple links

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Grailed-Push-Notifications
 
 This is a thing to send notifications when a new listing appears when given a specific filter
+
--- main.py
+++ main.py
@@ -1,10 +1,106 @@
-import time, sched
+import time, datetime
+import threading
 from selenium import webdriver
 from bs4 import BeautifulSoup as bs
 from colorama import Fore, Back, Style
 
+import os
+import sys
+import json
+
+import requests
+from flask import Flask, request
+
+# app = Flask(__name__)
+#
+#
+# @app.route('/', methods=['GET'])
+# def verify():
+#     # when the endpoint is registered as a webhook, it must echo back
+#     # the 'hub.challenge' value it receives in the query arguments
+#     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+#         if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+#             return "Verification token mismatch", 403
+#         return request.args["hub.challenge"], 200
+#
+#     return "Hello world", 200
+#
+#
+# @app.route('/', methods=['POST'])
+# def webhook():
+#     # endpoint for processing incoming messaging events
+#
+#     data = request.get_json()
+#     log(data)  # you may not want to log every incoming message in production, but it's good for testing
+#
+#     if data["object"] == "page":
+#
+#         for entry in data["entry"]:
+#             for messaging_event in entry["messaging"]:
+#
+#                 if messaging_event.get("message"):  # someone sent us a message
+#
+#                     sender_id = messaging_event["sender"]["id"]  # the facebook ID of the person sending you the message
+#                     recipient_id = messaging_event["recipient"][
+#                         "id"]  # the recipient's ID, which should be your page's facebook ID
+#                     message_text = messaging_event["message"]["text"]  # the message's text
+#
+#                     send_message(sender_id, "roger that!")
+#
+#                 if messaging_event.get("delivery"):  # delivery confirmation
+#                     pass
+#
+#                 if messaging_event.get("optin"):  # optin confirmation
+#                     pass
+#
+#                 if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+#                     pass
+#
+#     return "ok", 200
+#
+#
+# def send_message(recipient_id, message_text):
+#     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+#
+#     params = {
+#         "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+#     }
+#     headers = {
+#         "Content-Type": "application/json"
+#     }
+#     data = json.dumps({
+#         "recipient": {
+#             "id": recipient_id
+#         },
+#         "message": {
+#             "text": message_text
+#         }
+#     })
+#     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+#     if r.status_code != 200:
+#         log(r.status_code)
+#         log(r.text)
+#
+#
+# def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
+#     try:
+#         if type(msg) is dict:
+#             msg = json.dumps(msg)
+#         else:
+#             msg = str(msg).format(*args, **kwargs)
+#         print(u"{}: {}".format(datetime.now(), msg))
+#     except UnicodeEncodeError:
+#         pass  # squash logging errors in case of non-ascii text
+#     sys.stdout.flush()
+#
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
 # Config
+first_time = False
 url = "https://www.grailed.com/feed/rn0qT30h5A"
+# https://www.grailed.com/feed/E9xeJRem6w
 old_items = set()
 
 # Set Up
@@ -19,7 +115,7 @@ def get_listings():
     # threading.Timer(5.0, get_listings).start()
     print(Fore.YELLOW + "Checking" + Style.RESET_ALL)
 
-    global url, old_items
+    global url, old_items, first_time
     driver.get(url)
 
     html = driver.page_source
@@ -32,8 +128,11 @@ def get_listings():
             current_items.add(item.a.get("href"))
 
     diff = current_items.difference(old_items)
-    if diff:
+    if diff and first_time is not True:
+        print("New Items!!")
         print(diff)
+    else:
+        first_time = False
     old_items = current_items
 
 
