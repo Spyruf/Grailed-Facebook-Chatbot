9407d7349cc103bea07d9816c4cd6b0dde5f89d9
Rahul Batra <spyruf@gmail.com>
2017-12-25 22:12:56 -0500

    basic one check verison working

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import time
+import time, sched
 from selenium import webdriver
 from bs4 import BeautifulSoup as bs
 from colorama import Fore, Back, Style
 
 # Config
 url = "https://www.grailed.com/feed/rn0qT30h5A"
+old_items = set()
 
 # Set Up
 print(Fore.GREEN + "Start" + Style.RESET_ALL)
@@ -13,16 +14,25 @@ options = webdriver.ChromeOptions()
 options.add_argument('headless')
 driver = webdriver.Chrome(chrome_options=options)
 
-# Code
-driver.get(url)
 
-html = driver.page_source
-soup = bs(html, "html.parser")
+def get_listings():
+    global url, old_items
+    driver.get(url)
 
-row = soup.find_all("div", {"class": "row"})
+    html = driver.page_source
+    soup = bs(html, "html.parser")
+    listings = soup.find_all("div", class_="feed-item")
 
-for listing in row:
-    print(listing.div.a.get("href"))
+    current_items = set()
+    for item in listings:
+        if item.a is not None:
+            current_items.add(item.a.get("href"))
+
+    print(current_items.difference(old_items))
+    old_items = current_items
+
+
+get_listings()
 
 driver.quit()
 print(Fore.GREEN + "End" + Style.RESET_ALL)
