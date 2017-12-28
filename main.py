b415b8137b1d2e80663b7a3bd90b47a91a1c97c2
Rahul Batra <spyruf@gmail.com>
2017-12-28 17:44:07 -0500

    stuff

--- main.py
+++ main.py
@@ -16,8 +16,8 @@ app = Flask(__name__)
 
 # Config
 # os.environ[
-#     "PAGE_ACCESS_TOKEN"] = "EAAF5JrI1h00BALc1R1OrZC2NBSPS9UQtFbPkzHS9YXwZC9ZAizFA3s1luLH6EwqpwtTzFrGpMG4SpfqbZALGdUbVaqus8DKD9lt4WEhMTGPkb5Rjsc34xRsks05olIjTbSO0fZAQmwbyC6BajbrxsiAZA3HYZAqvTnlZChfNTMFLXT7gW9DAJmRd"
-# os.environ["VERIFY_TOKEN"] = "test_token"
+#     "PAGE_ACCESS_TOKEN"] = ""
+# os.environ["VERIFY_TOKEN"] = ""
 
 print(Fore.CYAN, "CONFIG:")
 print(Fore.CYAN, "PAGE_ACCESS_TOKEN: " + os.environ["PAGE_ACCESS_TOKEN"])
