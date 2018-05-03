ae54fdf7bddc90c05c6ddca0de1a66dec27b2e0c
Rahul Batra <spyruf@gmail.com>
2018-05-03 13:40:56 -0400

    temp fix logging for incorrect post request

--- main.py
+++ main.py
@@ -514,6 +514,7 @@ def send_message(recipient_id, message_text):
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
         log(r.status_code)
+        print(Fore.GREEN + r.text)
         # log(r.text) TODO
 
 
