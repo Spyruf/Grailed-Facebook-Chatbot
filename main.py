e962d760e5cd87352d67a1c09871673895fbb4f0
Rahul Batra <spyruf@gmail.com>
2018-05-09 03:26:25 -0400

    updated post method send

--- main.py
+++ main.py
@@ -500,8 +500,11 @@ def webhook():
 def send_message(recipient_id, message_text):
     # log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
 
+    url = "https://graph.facebook.com/v2.6/me/messages"
+
     params = {"access_token": os.environ["PAGE_ACCESS_TOKEN"]}
     headers = {"Content-Type": "application/json"}
+
     data = json.dumps({
         "recipient": {
             "id": recipient_id
@@ -511,11 +514,13 @@ def send_message(recipient_id, message_text):
             "text": message_text
         }
     })
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-    if r.status_code != 200:
-        log(r.status_code)
-        print(Fore.GREEN + r.text)
-        # log(r.text) TODO
+
+    # r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
+    response = requests.request("POST", url, data=data, headers=headers, params=params)
+
+    if response.status_code != 200:
+        log(Fore.GREEN + response.status_code + Fore.RESET)
+        print(Fore.GREEN + response.text + Fore.RESET)
 
 
 def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
